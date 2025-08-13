0ff6758de9bc9621e598268e715928927262de93
inxeoz <inxeoz@inxeoz.com>
2025-08-14 01:34:23 +0530

    global.css not affecting the css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+// import "../styles/index.css";
+import "../styles/globals.css";
 
 // Controlled mount
 export function mountReact(el: HTMLElement) {
@@ -14,8 +15,8 @@ export function mountReact(el: HTMLElement) {
 
 //uncomment this only when this project intend to run by project (self)
 //
- ReactDOM.createRoot(document.getElementById("root_itsupport")!).render(
-   <React.StrictMode>
-     <App />
-   </React.StrictMode>,
- );
+ReactDOM.createRoot(document.getElementById("root_itsupport")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
