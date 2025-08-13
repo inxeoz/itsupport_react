1657623334de0b95cf3c4771908858956e3d9ee5
inxeoz <inxeoz@inxeoz.com>
2025-08-13 15:00:09 +0530

    working

--- vite.config.ts
+++ vite.config.ts
@@ -3,40 +3,10 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
 
-// const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": resolve(__dirname, "./src"),
-//     },
-//   },
-//   define: {
-//     "process.env": {},
-//   },
-//   build: {
-//     outDir: "../public/static_ui",
-//     emptyOutDir: true,
-//     lib: {
-//       entry: "./src/main.tsx",
-//       formats: ["es"],
-//       fileName: () => `main.js`,
-//     },
-//     rollupOptions: {
-//       external: [],
-//       output: {
-//         entryFileNames: `main.js`,
-//         assetFileNames: `main.css`,
-//       },
-//     },
-//   },
-// });
-
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: "/assets/tsx_frappe/static_ui/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
@@ -48,5 +18,12 @@ export default defineConfig({
   build: {
     outDir: "../public/static_ui",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/main.js", // no hash
+        chunkFileNames: "assets/[name].js", // no hash for chunks
+        assetFileNames: "assets/[name].[ext]", // no hash for CSS/images
+      },
+    },
   },
 });
