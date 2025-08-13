54667cc9ef809fed2bb06ff32d7d1376ce2bb927
inxeoz <inxeoz@inxeoz.com>
2025-08-13 14:51:17 +0530

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,40 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+// const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": resolve(__dirname, "./src"),
+//     },
+//   },
+//   define: {
+//     "process.env": {},
+//   },
+//   build: {
+//     outDir: "../public/static_ui",
+//     emptyOutDir: true,
+//     lib: {
+//       entry: "./src/main.tsx",
+//       formats: ["es"],
+//       fileName: () => `main.js`,
+//     },
+//     rollupOptions: {
+//       external: [],
+//       output: {
+//         entryFileNames: `main.js`,
+//         assetFileNames: `main.css`,
+//       },
+//     },
+//   },
+// });
+
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  base: "/assets/tsx_frappe/static_ui/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
@@ -18,17 +48,5 @@ export default defineConfig({
   build: {
     outDir: "../public/static_ui",
     emptyOutDir: true,
-    lib: {
-      entry: "./src/main.tsx",
-      formats: ["es"],
-      fileName: () => `main.js`,
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        entryFileNames: `main.js`,
-        assetFileNames: `main.css`,
-      },
-    },
   },
 });
